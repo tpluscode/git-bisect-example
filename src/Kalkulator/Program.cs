97a4e4655bd25efb8422798e596a981d3a6401f4
tpluscode <tpluscode@users.noreply.github.com>
2017-02-19 11:58:01 +0100

    będzie liczenie unarnych funkcji

--- src/Kalkulator/Program.cs
+++ src/Kalkulator/Program.cs
@@ -10,14 +10,28 @@ namespace Kalkulator
     {
         public static void Main(string[] args)
         {
-            decimal result;
-            var op = args[1];
-            var left = decimal.Parse(args[0]);
-            var right = decimal.Parse(args[2]);
+            decimal? result = null;
 
-            result = CalculateBinary(op, left, right);
+            if (args.Length == 3)
+            {
+                var op = args[1];
+                var left = decimal.Parse(args[0]);
+                var right = decimal.Parse(args[2]);
+                result = CalculateBinary(op, left, right);
+            }
+            else if(args.Length == 2)
+            {
+                var op = args[0];
+                var operand = decimal.Parse(args[1]);
+                result = CalculateUnary(op, operand);
+            }
 
             Console.WriteLine($"Wynik: {result}");
+        }
+
+        private static decimal? CalculateUnary(string op, object operand)
+        {
+            throw new NotImplementedException();
         }
 
         private static decimal CalculateBinary(string op, decimal left, decimal right)

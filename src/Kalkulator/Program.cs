6fdea754d6c2f3476790d89b3becaf49baffcfd8
tpluscode <tpluscode@users.noreply.github.com>
2017-02-19 11:26:40 +0100

    init repo

---
+++ src/Kalkulator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var op = args[1];
+            var left = decimal.Parse(args[0]);
+            var right = decimal.Parse(args[2]);
+
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine(left + right);
+                    break;
+            }
+        }
+    }
+}
